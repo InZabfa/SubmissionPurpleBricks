96bd8a297fbd32eceba639c6d7534563695b9d43
Inese <Inese.Zabfa@outlook.com>
2020-01-18 15:50:36 +0000

    Challange 1

--- testapp.Core/ViewModels/About/AboutViewModel.cs
+++ testapp.Core/ViewModels/About/AboutViewModel.cs
@@ -1,6 +1,18 @@
-﻿namespace testapp.Core.ViewModels.About
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace testapp.Core.ViewModels.About
 {
     public class AboutViewModel : BaseViewModel
     {
+        //checks for when the button is pressed in the AboutPage.xaml
+        public ICommand VisitWebsiteCommand { get; }
+
+        public AboutViewModel()
+        {
+            //Opens the website once the button on the page is pressed
+            VisitWebsiteCommand = new Command(() => Device.OpenUri(new Uri("http://www.purplebricks.com")));
+        }
     }
 }
